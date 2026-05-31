358bd61c527efea9b16d5ae7e860715ed1521f88
alnwm162-jpg <alnwm162-jpg@users.noreply.github.com>
2026-05-31 12:11:19 +0000

    deploy: 32a30af3d994981d9d93483f4c23a51c1c106104

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "405086887" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "176941516" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
