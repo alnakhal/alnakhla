794e543d20945605c5364be40d4e2f65178257f7
Hayman <alnwm162@gmail.com>
2026-06-03 19:03:20 +0300

    تحديث جديد6

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:al_mustalazimat_al_iraqiya/main.dart';
+import 'package:mustalazimat_al_nakhla/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
