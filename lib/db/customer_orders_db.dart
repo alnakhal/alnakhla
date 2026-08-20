fc429dd75798a362a1604661c9559e8cc935bbca
Hayman <alnwm162@gmail.com>
2026-08-20 14:50:54 +0300

    Fix web orders database storage

--- lib/db/customer_orders_db.dart
+++ lib/db/customer_orders_db.dart
@@ -1,6 +1,7 @@
-import 'package:sembast/sembast_io.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
 import '../models/customer_order_model.dart';
+import 'sembast_database_factory_io.dart'
+    if (dart.library.html) 'sembast_database_factory_web.dart';
 
 class CustomerOrdersDatabase {
   static final CustomerOrdersDatabase instance = CustomerOrdersDatabase._init();
@@ -15,10 +16,7 @@ class CustomerOrdersDatabase {
   }
 
   Future<Database> _initDB(String dbName) async {
-    final appDocDir = await getApplicationDocumentsDirectory();
-    final dbPath = '${appDocDir.path}/$dbName';
-    final db = await databaseFactoryIo.openDatabase(dbPath);
-    return db;
+    return databaseFactory.openDatabase(dbName);
   }
 
   final _orderStore = intMapStoreFactory.store('customer_orders');
@@ -48,7 +46,8 @@ class CustomerOrdersDatabase {
 
   Future<CustomerOrderModel?> getOrderByNumber(String orderNumber) async {
     final db = await database;
-    final finder = Finder(filter: Filter.equals('order_number', orderNumber), limit: 1);
+    final finder =
+        Finder(filter: Filter.equals('order_number', orderNumber), limit: 1);
     final records = await _orderStore.find(db, finder: finder);
     if (records.isEmpty) return null;
     final map = Map<String, dynamic>.from(records.first.value);
@@ -68,13 +67,11 @@ class CustomerOrdersDatabase {
       finder = Finder(sortOrders: [SortOrder('created_at', false)]);
     }
     final records = await _orderStore.find(db, finder: finder);
-    return records
-        .map((record) {
-          final map = Map<String, dynamic>.from(record.value);
-          map['id'] = record.key;
-          return CustomerOrderModel.fromMap(map);
-        })
-        .toList();
+    return records.map((record) {
+      final map = Map<String, dynamic>.from(record.value);
+      map['id'] = record.key;
+      return CustomerOrderModel.fromMap(map);
+    }).toList();
   }
 
   Future<void> deleteOrder(int id) async {
